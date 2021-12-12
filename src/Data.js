fb2ac43631b89b06aa5be88f85c05d6a58f08d33
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-12-12 13:02:04 +0100

    refactor: added src/Data.js (#3)

---
+++ src/Data.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line
+export default {
+    "version": "v0.25.0",
+    "namespaces": ["Prelude"],
+    "classes": [],
+    "enums": [],
+    "typeAliases": [],
+    "defs": []
+}
